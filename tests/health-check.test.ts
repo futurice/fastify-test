07617623b8c3b1c52ddaa51661d6f7483e6ecebe
Daniel Saarimäki <daniel.saarimaki@futurice.com>
2021-11-07 19:42:26 +0200

    Add a simple health check test

---
+++ tests/health-check.test.ts
@@ -0,0 +1,27 @@
+import { FastifyInstance } from 'fastify';
+import { expect } from 'chai';
+
+import { build } from '../src/app';
+
+describe('Health check', () => {
+  let server : FastifyInstance;
+
+  before(() => {
+    server = build({
+      logger: { prettyPrint: true },
+    }, false);
+  });
+
+  after(async () => {
+    await server.close();
+  })
+
+  it('should succeed', async () => {
+    const response = await server.inject({
+      method: 'GET',
+      url: '/',
+    });
+
+    expect(response.statusCode).to.equal(200);
+  });
+});
