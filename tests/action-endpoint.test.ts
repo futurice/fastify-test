f55eca9a6bbbd47af28e9bcc1cd5f88e31a86cf9
Daniel Saarimäki <daniel.saarimaki@futurice.com>
2021-11-09 20:11:52 +0200

    Fix the feed item generation endpoint

--- tests/action-endpoint.test.ts
+++ tests/action-endpoint.test.ts
@@ -1,14 +1,21 @@
 import { FastifyInstance } from 'fastify';
 import { expect } from 'chai';
+import { sql } from 'slonik';
 
+import { getPool } from '../src/plugins/db';
 import { build } from '../src/app';
 import config from '../src/config';
 import { ActionType } from '../src/routes/endpoints/v1/action/schemas';
 
+const TEST_USER = 'fef01237-6f6a-4b12-83b5-18874624ba54';
+
 describe('Action endpoint', () => {
   let server : FastifyInstance;
 
-  before(() => {
+  before(async () => {
+    const pool = getPool();
+    await pool.query(sql`TRUNCATE comment, action, feed_item`);
+  
     server = build({
       logger: { prettyPrint: true },
     }, false);
@@ -18,18 +25,21 @@ describe('Action endpoint', () => {
     await server.close();
   })
 
-  it.only('should generate a new feed item when sending a message', async () => {
+  it('should generate a new feed item when sending a message', async () => {
     const createActionResponse = await server.inject({
       method: 'POST',
       url: '/api/v1/action/',
       headers: {
         'x-api-token': config.TOKEN,
+        'x-user-uuid': TEST_USER,
       },
       payload: {
         text: 'Test message text',
         type: ActionType.TEXT,
       },
     });
+
+    console.log(createActionResponse.json());
 
     expect(createActionResponse.statusCode).to.equal(200);
 
@@ -38,11 +48,27 @@ describe('Action endpoint', () => {
       url: '/api/v1/feed/',
       headers: {
         'x-api-token': config.TOKEN,
+        'x-user-uuid': TEST_USER,
       },
     });
 
     expect(getFeedResponse.statusCode).to.equal(200);
+  
     const feedItems = getFeedResponse.json();
-    console.log(JSON.stringify(feedItems));
+    expect(feedItems.length).to.equal(1);
+    expect(feedItems[0].uuid).to.be.a('string');
+    expect(feedItems[0].createdAt).to.be.a('string');
+    expect(feedItems[0].updatedAt).to.be.a('string');
+    expect(feedItems[0]).to.deep.include({
+      type: 'TEXT',
+      text: 'Test message text',
+      image: null,
+      isSticky: false,
+      commentCount: 0,
+      author: {
+        name: 'hessu',
+        guild: 'TiTe',
+      }
+    })
   });
 });
